5dea585af91888b53118e8874c5f18438ce137c1
ValentaCodes <valentacodes@gmail.com>
2022-09-19 23:32:35 -0600

    Added Javascript to project

---
+++ assets/script.js
@@ -0,0 +1,19 @@
+var topics = ["HTML", "CSS", "Git", "Javascript"];
+
+for(var topic = 0; topic < topics.length; topic++) {
+    console.log(topics[topic]);
+}
+
+/*
+if (topic ==='HTML'){
+    console.log("Let's study HTML!");
+}   else if (topic === 'CSS'){
+    console.log("Let's study CSS!");
+}   else if (topic ==='Git'){
+    console.log("Let's study Git!");
+}   else if (topic === 'Javascript'){
+    console.log("Let's study Javascript!");
+}   else {
+    console.log("Please try again!");
+} 
+*/
